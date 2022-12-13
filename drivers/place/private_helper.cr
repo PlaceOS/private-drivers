dd672050ba99e51f86592da8c1adc64a6c4cd144
Stephen von Takach <steve@place.technology>
2022-12-13 14:03:57 +1100

    chore(place\private_helper): test edge control of branch module

--- drivers/place/private_helper.cr
+++ drivers/place/private_helper.cr
@@ -5,6 +5,10 @@ class Place::PrivateHelper < PlaceOS::Driver
   descriptive_name "PlaceOS Test Helper"
   generic_name :Helper
 
+  def on_a_branch
+    "hello branch"
+  end
+
   def used_for_place_testing
     logger.debug { "this will be propagated to backoffice!" }
     "you can delete this file"
